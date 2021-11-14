8e0f3f45a25b4a6f4e071b97d977d5394e392d93
Rae J. Khan <raekhan18@gmail.com>
2021-11-14 18:30:53 +0000

    Add general stats, such as products count and categories count in database

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => ENV['USERNAME'], :password => ENV['PASSWORD']
 
   def show
+    @products_count = Product.count
+    @categories_count = Product.count
   end
 end
